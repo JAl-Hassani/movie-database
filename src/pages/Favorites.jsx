dde66f544cc659f10f7e9c578986087448a8d6f8
Janan Al-Hassani <janan.alhassani@gmail.com>
2025-06-26 12:46:11 -0400

    Add routes to home and favorite pages

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No favorites</h2>
+            <p>Start adding your favorite movies here!</p>
+        </div>
+    )
+}
+
+export default Favorites
